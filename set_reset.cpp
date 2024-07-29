9ad2ff4471a490cc92b2ddf1e78943938ee56565
DevkarthikSuresh <devkarthik300@gmail.com>
2024-07-29 20:45:39 +0530

    Add files via upload

---
+++ set_reset.cpp
@@ -0,0 +1,27 @@
+//Name - Devarthik Suresh
+//PRN - 23070123045
+//Aim - Performing Bitwise operations
+#include <iostream>
+using namespace std;
+
+int main() { int a,i,set,reset;
+    // Write C++ code here
+    cout << "Enter the number";
+    cin >> a;
+    cout << "Enter the places to shift";
+    cin >> i;
+    set = a|(1<<i);
+    reset = a&(~(1<<i));
+    cout << "reset: "<<reset<< endl;
+    cout << "set: "<<set;
+
+
+
+    return 0;
+}
+/* Output-
+ * Enter the number5
+Enter the places to shift2
+reset: 1
+set: 5
+ */
